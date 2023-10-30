eead5761ddc33dc67eb1a8602c80651dc5b0e52f
Luong Nguyen Khoa <cucdaunho@gmai.com>
2023-10-30 14:12:05 +0700

    initial set up

---
+++ build/CMakeFiles/cs202.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "/Users/macbookair/Desktop/CS202-Meow-Invaders/cs202"
+  "/Users/macbookair/Desktop/CS202-Meow-Invaders/cs202.pdb"
+  "CMakeFiles/cs202.dir/src/GlobalVars.cpp.o"
+  "CMakeFiles/cs202.dir/src/GlobalVars.cpp.o.d"
+  "CMakeFiles/cs202.dir/src/Menu.cpp.o"
+  "CMakeFiles/cs202.dir/src/Menu.cpp.o.d"
+  "CMakeFiles/cs202.dir/src/main.cpp.o"
+  "CMakeFiles/cs202.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cs202.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
